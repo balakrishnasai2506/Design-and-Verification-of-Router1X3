d63da94698ed7e5135b5e4fc4f0a0dce2fb1404b
Bala Krishna Sai <brucewayne25062000@gmail.com>
2025-05-19 08:02:06 +0530

    Update source_seqs.sv

--- wr_agt_top/source_seqs.sv
+++ wr_agt_top/source_seqs.sv
@@ -122,37 +122,3 @@ task large_packet::body();
 		finish_item(req);
 //	end
 endtask
-
-//----------------------------Error Packet------------------------------
-class error_packet extends source_seqs;
-        `uvm_object_utils(error_packet)
-        bit [1:0] addr; //helps in getting  the destination address from test
-
-        //Methods
-        extern function new(string name = "error_packet");
-        extern task body();
-endclass
-
-
-//---------------------Constructor "new"--------------------------
-function error_packet::new(string name = "error_packet");
-        super.new(name);
-endfunction
-
-
-//----------------------Body()-----------------------------------
-task error_packet::body();
-                req = source_xtn::type_id::create("req"); //Create an object for transaction class
-                //Get the addr from uvm_config_db
-                if(!uvm_config_db#(bit[1:0])::get(null, get_full_name(), "addr", addr))
-                        `uvm_fatal("ERROR_PACKET","Couldn't get the addr from uvm_config_db... Did u set it right???")
-                start_item(req);
-                assert(req.randomize() with     {
-                                        header [1:0] == addr;
-                                        header [7:2] dist {[1:20] := 1, [21:40] := 1, [41:63] := 1};
-					payload[0] == header ^ (1 << $urandom_range(0,7)) ^ (^header);
-                                                });
-                `uvm_info("ERROR_PACKET_SEQUENCE",$sformatf("printing from sequence \n %s", req.sprint()),UVM_HIGH)
-                finish_item(req);
-endtask
-
